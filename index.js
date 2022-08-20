801cb0446e869d9f58edc646092acc00afe14c6e
jfmam <jfmam@naver.com>
2022-08-20 22:00:06 +0900

    create generate sitemap

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const prettier = require('prettier');
+const express = require('express')
+const boardData = require('./board-data.json');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const formatted = sitemap => prettier.format(sitemap, { parser: 'html' });
+    const postList = `
+    ${boardData
+        .map(v => {
+        return `
+            <url>
+            <loc>${process.env.DOMAIN}/index/${v['unique id']}</loc>
+            <lastmod>${new Date(v['Modified Date']).toISOString()}</lastmod>
+            </url>`
+        })
+        .join('')
+    }
+`
+    const generatedSitemap = `
+	<?xml version="1.0" encoding="UTF-8"?>
+  	<urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  >
+  <url>
+    <loc>${process.env.DOMAIN}</loc>
+    <priority>1</priority>
+  </url>
+    ${postList}
+  </urlset>
+  `;
+  const formattedSitemap = [formatted(generatedSitemap)];
+ 
+  fs.writeFileSync('sitemap.xml', formattedSitemap.toString(), 'utf-8')
+
+
+app.listen(8080, () => {
+    console.log('generate sitemap');
+});

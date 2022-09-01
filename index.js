38aafc8dc281b290f615ac840ccf8a6f019ccd46
jfmam <jfmam@naver.com>
2022-09-01 11:44:04 +0900

    add request-partner, request-estimate url in sitemap

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const prettier = require('prettier');
 const express = require('express')
 const boardData = require('./board-data.json');
 const fs = require('fs');
-const path = require('path');
 
 const app = express();
 const formatted = sitemap => prettier.format(sitemap, { parser: 'html' });
     const postList = `
     ${boardData
         .map(v => {
-        return `
-            <url>
+        return `<url>
             <loc>${process.env.DOMAIN}/index/${v['unique id']}</loc>
             <lastmod>${new Date(v['Modified Date']).toISOString()}</lastmod>
             </url>`
@@ -26,6 +24,14 @@ const formatted = sitemap => prettier.format(sitemap, { parser: 'html' });
   >
   <url>
     <loc>${process.env.DOMAIN}</loc>
+    <priority>1</priority>
+  </url>
+   <url>
+    <loc>${process.env.DOMAIN}/request_estimate</loc>
+    <priority>1</priority>
+  </url>
+   <url>
+    <loc>${process.env.DOMAIN}/request_partner</loc>
     <priority>1</priority>
   </url>
     ${postList}
